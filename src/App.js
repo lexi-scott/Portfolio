3e9aa5756d7ed68977a81f895d8bb97c1853ff36
slexicon <lexiscott0101@gmail.com>
2023-03-30 13:02:47 -0700

    update about me

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     email: "lexiscott0101@gmail.com",
     availability: "Open for work",
     brand:
-      "My life experiences, attention to detail, friendly demonear and drive have lead me to become a versatile and adaptive full-stack web developer with an enthusiastic energy while bringing projects to life. I enjoy the creativity of imagining a web application, then tapping into my design skills when developing the frontend. After, I apply my analytical side when conceptualizing data collection, server routes and debugging. After dedicating myself in the legal, education and wine industries, I am ecstatic to become involved in software development!",
+      "My life experiences, attention to detail, friendly demonear and drive have lead me to become a versatile and adaptive full-stack web developer. I appraoch building web applications with an enthusiastic energy - I am a big fan of having fun and a good laugh. However, I take a lot of pride in my work and my word. I've been coding for just over a year now and looking at my first projects to what I can make today gets me very excited for my own future. After dedicating myself in the legal, education and wine industries, I am ecstatic to become involved in software development!",
   };
 
   return (
